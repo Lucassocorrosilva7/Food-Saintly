3d91006fb8a412c4a0f413a789593b14e44cca10
lukecode <lucassocorrosilva@gmail.com>
2022-10-20 01:35:28 -0300

    Add novas funções

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector("#mobile-menu")
 const menuLinks = document.querySelector(".navbar__menu")
 const navLogo = document.querySelector("#navbar__logo")
+const link = document.querySelectorAll(".navbar__menu--link")
 const body = document.querySelector('body')
 
 
@@ -10,4 +11,15 @@ const mobileMenu = () => {
     body.classList.toggle('active')
 }
 
+const removeMobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    body.classList.toggle('active')
+}
+
+
 menu.addEventListener('click', mobileMenu)
+
+link.forEach(el =>  {
+    el.addEventListener("click", removeMobileMenu)
+});
